575e24c292bcf37d6d971418029f9271bedeac00
john1341100 <115842024+john1341100@users.noreply.github.com>
2023-04-05 10:30:52 -1000

    finsihed second level
    could add multiple arrows

--- Level2.js
+++ Level2.js
@@ -1,29 +1,39 @@
-function arrow(){
+function arrow() {
     this.positive = 10
     this.negative = -10
     this.hit = false
     //if refer to stuff inside you, use this, but if calling something outside of object, use object name instead
     this.xValue = 20
-    this.yValue = h/2
+    this.yValue = h / 2
     this.x = this.xValue
     this.y = this.yValue
     this.enemyX = w
     this.enemyY = h - 200
     this.velocityE = -10
-    this.loop = function(){
-        this.showEnemy = function(){
-            fill('blue')
-            rect(this.enemyX, this.enemyY, 100, 100)
-            this.burn = document.getElementById('fireball')
-        }
-    this.enemyMove = function (){
-        this.enemyY += this.velocityE
-        if(this.enemyY < -100){
-            this.enemyY = h-100
-            this.enemyX =random(0, w-200)
-            this.fireball.style.left = this.enemyY
+    this.arrow = document.getElementById('arrow')
+    this.loop = function () {
+        this.enemyMove = function () {
+            this.enemyY += this.velocityE
+            if (this.enemyY < -100) {
+                this.enemyY = h - 100
+                this.enemyX = random(0, w - 200)
+            }
         }
     }
-}
-    
+    this.showEnemy = function () {
+            fill('')
+            rect(this.enemyX, this.enemyY, 50, 170)
+            this.arrow = document.getElementById('arrow')
+            this.arrow.style.top = this.enemyY+20
+            this.arrow.style.left = this.enemyX-50
+        }
+    this.collide = function(){
+        this.hit = collideRectCircle(this.enemyX, this.enemyY, 50, 170,doom.x, doom.y, 40 )
+        if(this.hit === true){
+            doom.x = 50
+            doom.y = h/2
+            console.log('hi')
+        }
+    }
+
 }
