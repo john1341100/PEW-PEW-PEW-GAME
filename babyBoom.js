82319b8c32699729fbe15a96c063dc87cf4515eb
john1341100 <115842024+john1341100@users.noreply.github.com>
2023-04-12 10:32:00 -1000

    exploding babies

---
+++ babyBoom.js
@@ -0,0 +1,17 @@
+function boom(){
+    this.positive = 10
+    this.negative = -10
+    this.hit = false
+    //if refer to stuff inside you, use this, but if calling something outside of object, use object name instead
+    this.xValue = 20
+    this.yValue = h/2
+    this.x = this.xValue
+    this.y = this.yValue
+    //gravity
+    //velocity
+    this.velocity = 0
+    //enemy code
+    this.enemyX = w - 100
+    this.enemyY = h - 200
+    this.velocityE = -10
+}
